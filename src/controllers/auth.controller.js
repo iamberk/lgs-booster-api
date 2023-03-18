ef730867656b3a8585eacc954e91bbf27e54ddfa
berk <berk-karatas@protonmail.com>
2023-03-18 17:28:06 +0300

    documentation and Response validation added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const user = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const APIError = require('../utils/errors');
+const Response = require('../utils/response');
 
 const login = async (req, res) => {
     console.log(req.body);
@@ -18,29 +19,14 @@ const register = async (req, res) => {
 
     req.body.password = await bcrypt.hash(req.body.password , 10);
 
-    try {
-        const userSave = new user(req.body);
-        await userSave.save()
-            .then(response => {
-                return res.status(201).json({ 
-                    message: 'User created successfully',
-                    success: true,
-                    data: response 
-                });
-            },)
-            .catch(err => {
-                return res.status(400).json({ 
-                    message: 'User not created',
-                    success: false,
-                    data: err 
-                });
-            });
-            
-    } catch (err) {
-        console.log(err);
-    }
-
-    console.log(req.body);
+    const userSave = new user(req.body);
+    await userSave.save()
+        .then(data => {
+            return new Response(data, 'User created successfully').created(res);
+        },)
+        .catch(err => {
+            throw new APIError(err.message, 400)
+        });
 }
 
 module.exports = {  login, register }   
