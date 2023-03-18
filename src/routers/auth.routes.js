2d8873c5f229f7ebf7cfa87ac0a113b2b22f4953
berk <berk-karatas@protonmail.com>
2023-03-18 18:10:35 +0300

    register validation with joi

--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-
+const AuthValidation = require('../middlewares/validations/auth.validation'); 
 const {login, register} = require("../controllers/auth.controller");
 
 
-router.post('/register', register);
+router.post('/register', AuthValidation.register, register);
 
 router.post('/login', login);
 
