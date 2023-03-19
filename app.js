692a8ce91e7ad6afb6edf4f6f1dadf8e9d9ebe25
berk <berk-karatas@protonmail.com>
2023-03-19 10:54:20 +0300

    Avoid Injection with mongo-sanitize

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5001;
 const errorHandlerMiddleware = require('./src/middlewares/errorHandler');
 const cors = require('cors');
 const corsOptions = require('./src/helpers/corsOptions');
+const mongoSanitize = require('express-mongo-sanitize');
 
 //Middlewares
 app.use(express.json());
@@ -14,6 +15,12 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true , parameterLimit: 50000}));
 
 app.use(cors(corsOptions));
+
+app.use(
+    mongoSanitize({
+      replaceWith: '_',
+    }),
+  );
 
 const router = require('./src/routers/index');
 app.use('/api', router);
