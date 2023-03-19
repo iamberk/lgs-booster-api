ad06ffd0c785bdf4ab2c757befae7a945ee79c26
berk <berk-karatas@protonmail.com>
2023-03-19 11:40:08 +0300

    image upload with multer

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const port = process.env.PORT || 5001;
 const errorHandlerMiddleware = require('./src/middlewares/errorHandler');
 const cors = require('cors');
+const path = require('path');
 const corsOptions = require('./src/helpers/corsOptions');
 const mongoSanitize = require('express-mongo-sanitize');
 
@@ -13,6 +14,9 @@ const mongoSanitize = require('express-mongo-sanitize');
 app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true , parameterLimit: 50000}));
+
+app.use(express.static(path.join(__dirname, "public")))
+app.use("/uploads", express.static(__dirname))
 
 app.use(cors(corsOptions));
 
